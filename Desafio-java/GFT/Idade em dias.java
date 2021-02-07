7e440a788f02009ff1f4a90a4f1c804ad8c17632
Richardson Clayton <70027084+Richardson-clayton@users.noreply.github.com>
2021-02-06 21:28:09 -0300

    Atualização de códigos
    Correção nos códigos do desafios 1 e 2.

--- Desafio-java/GFT/Idade em dias.java
+++ Desafio-java/GFT/Idade em dias.java
@@ -8,7 +8,7 @@ public class Main {
     int anos = idadeDias / 362;
     int meses = ((idadeDias % 365) / 30);
     int dias = ((idadeDias % 365) % 30);
-    System.out.println(anos + " anos(s)");
+    System.out.println(anos + " ano(s)");
     System.out.println(meses + " mes(es)");
     System.out.println(dias + " dia(s)");
   }
