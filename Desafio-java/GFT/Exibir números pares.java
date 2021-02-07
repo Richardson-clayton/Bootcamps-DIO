df1f0dd3e932e5c1a55c2caba7e4d34170ecf2fa
Richardson Clayton <70027084+Richardson-clayton@users.noreply.github.com>
2021-02-06 21:12:33 -0300

    Código desafio 1 dio
    Criando um programa que exibi numero pares em java.

---
+++ Desafio-java/GFT/Exibir números pares.java
@@ -0,0 +1,18 @@
+/* Crie um programa que leia um número e mostre os números pares até esse número, inclusive ele mesmo.*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    String entrada;
+    BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+    entrada = br.readLine();
+    for (int i = 2; i < Integer.parseInt(entrada); i++){
+      if (i % 2 == 0){
+        System.out.println(i);
+      }
+    }
+  }
+} 
