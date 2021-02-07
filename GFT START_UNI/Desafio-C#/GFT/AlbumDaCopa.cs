f58b277a4f06fc2a6500d129a60c4e1465098e3a
Richardson Clayton <70027084+Richardson-clayton@users.noreply.github.com>
2021-02-06 22:11:35 -0300

    Desafio C#

---
+++ GFT START_UNI/Desafio-C#/GFT/AlbumDaCopa.cs
@@ -0,0 +1,32 @@
+﻿/*Dados o número total de espaços e figurinhas do álbum, e uma lista das figurinhas já compradas (que pode conter figurinhas repetidas), o seu desafio é determinar quantas figurinhas faltam para completar o álbum.*/
+
+using System;
+using System.Linq;
+
+class Program {
+  
+    static void Main(string[] args) {
+        int numeroDeFigurinhas = Int32.Parse(Console.ReadLine());
+        int numeroDeFigurinhasCompradas = Int32.Parse(Console.ReadLine());
+        int totalDeFigurinhas = 0;
+
+        int[] albumDeFigurinha = new int[numeroDeFigurinhas];
+
+        
+            // IMPLEMENTE AQUI A SOLUÇÃO
+        for (int i = 0; i < numeroDeFigurinhasCompradas; i++) {
+            int figurinha = int.Parse(Console.ReadLine());
+            if (!albumDeFigurinha.Contains(figurinha)) {
+                albumDeFigurinha[i] = figurinha;
+                numeroDeFigurinhas -= 1;
+            }
+            if (numeroDeFigurinhas == 0) {
+                break;
+            }
+            }
+
+        totalDeFigurinhas = numeroDeFigurinhas;
+
+        Console.WriteLine(totalDeFigurinhas);
+    }
+}
