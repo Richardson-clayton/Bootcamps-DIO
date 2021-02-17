922b431e6a9117a0019d1ec31ac7d02486278f95
Richardson Clayton <70027084+Richardson-clayton@users.noreply.github.com>
2021-02-17 20:38:41 -0300

    Organização

--- Avanade Angular Developer/cod_JavaScript/README.md
+++ Avanade Angular Developer/cod_JavaScript/README.md
@@ -1,4 +1,2 @@
-## Aula de javaScript
-
-introdução ao JavaScript
--> index_js = atividade de imteração do js com html.
+# aula-javascript
+ Introdução a JavaScript
--- Avanade Angular Developer/cod_JavaScript/js/main.js
+++ Avanade Angular Developer/cod_JavaScript/js/main.js
@@ -1,1 +1,50 @@
-alert("Esse é atividade de aula de js");
+//Primeira aula de JS
+
+/*var nome = "Richardson Clayton";
+var frase = "O japão é melhor time do mundo.";
+alert("Bem vindo " + nome);
+//alert("Leio o testo que fala do JS");
+console.log(nome);// verifica na inspeção do navegador, trabalha junto ao criente.
+console.log(frase.replace("japão", "Brasil"))
+alert("O Brasil é o melhor time do mundo.")*/
+
+//Aula 2 de JS - Array e dicionários
+
+/*var lista = ["eu", "tu", "ele" ];
+lista.push = ("nós");
+//lista.pop();
+console.log(lista)
+console.log(lista.toLocaleString())
+console.log(lista.join(" - "))
+
+var fruta = [{nome: "pera", cor: "branco"}, {nome: "uva", cor: "preta"}]
+console.log(fruta);
+alert(fruta[1].nome); */
+
+//Aula 3 de Js - condicionais, laços de repetição e Date
+
+/*var idade = prompt("Qual sua idade? ");
+if(idade >= 18){
+    alert("Maior de idade.");
+}else{
+    alert("Menor de idade.");
+};
+/*
+var count = 0;
+while (count <=5){
+    console.log(count);
+    count = count + 1;
+};
+
+var count;
+for(count=1; count <=5; count++){
+    alert(count);
+};*/
+
+/*var d = new Date ();
+alert(d);
+
+var n1 = 1;
+var n2 = 2;
+console.log(n1 + n2 + "1425 "-"+1519");*/
+
